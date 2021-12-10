a63d7d0c0d2c8a80d82b31ace0c1bb09add7bf4e
Khamza Habibullah <habibullahkhamza@gmail.com>
2021-12-10 20:30:48 +0400

    fixes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const toggleButton = document.querySelector('.toggle-button');
-const navbarItems = document.querySelector('.navigation__items');
-const navbarButton = document.querySelector('.navigation__button');
-
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarItems = document.getElementsByClassName('navigation__items')[0];
+const navbarButton = document.getElementsByClassName('navigation__button')[0];
+console.log(toggleButton);
 toggleButton.addEventListener('click', () => {
-  navbarItems.classList.toggle('active');
   navbarButton.classList.toggle('active');
+  navbarItems.classList.toggle('active');
 });
